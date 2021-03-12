7cc9ad4d1e7d53ecbf4eb18d176c328b7ab594c8
RashedRifat <59259308+RashedRifat@users.noreply.github.com>
2021-03-12 13:19:14 -0500

    updated
    liveCoding examples!

---
+++ tutorial2/liveCoding.py
@@ -0,0 +1,6 @@
+# Let's do a live coding session together, crytaliizing some of the things that we have learned so far 
+# Your Task: count the number of unique values within a list and store how many copies there are of each 
+# value in a dictonary 
+
+value_list = [1,2,3,2,3,3,4,5,6,4,5,6,4,5,6,4,5,6,5,6,6]
+value_counts = dict()
